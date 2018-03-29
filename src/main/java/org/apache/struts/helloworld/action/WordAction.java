69ba31a2f7704865415b6811abecaf02e2626338
Doug Parker <dougparker16@gmail.com>
2018-03-28 21:47:27 -0400

    getAllWords() working

--- src/main/java/org/apache/struts/helloworld/action/WordAction.java
+++ src/main/java/org/apache/struts/helloworld/action/WordAction.java
@@ -2,7 +2,9 @@ package org.apache.struts.helloworld.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts.helloworld.model.Word;
+import org.apache.struts.helloworld.model.Words;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.ArrayList;
 
 public class WordAction extends ActionSupport {
 
@@ -10,6 +12,7 @@ public class WordAction extends ActionSupport {
     private String id;
 
     private Word word;
+    private Words words;
 
     // helpers for request arguments
     public void setId(String id){
@@ -34,7 +37,15 @@ public class WordAction extends ActionSupport {
         throw new NotImplementedException();
     }
 
+    public String getAll() throws Exception {
+        words = new Words();
+        words.populateWordList();
+
+        return SUCCESS;
+    }
+
     // getter
     public Word getWord(){ return word; }
+    public ArrayList<Word> getWords(){ return words.getWords(); }
 
 }
---
+++ src/main/java/org/apache/struts/helloworld/model/Words.java
@@ -0,0 +1,67 @@
+ 
+package org.apache.struts.helloworld.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * Model class that stores a message.
+ * @author Bruce Phillips
+*/
+ 
+public class Words {
+
+    Connection con;
+    String filter, sortField, sortOrder;
+    ArrayList<Word> words;
+
+    public void setFilter(String filter){
+        this.filter = filter;
+    }
+
+    public void setSortField(String sortField){
+        this.sortField = sortField;
+    }
+
+    public void setSortOrder(String sortOrder){
+        this.sortOrder = sortOrder;
+    }
+
+    public void populateWordList() throws Exception{
+        this.words = new ArrayList<Word>();
+
+        Class.forName("com.mysql.jdbc.Driver");
+        this.con = DriverManager.getConnection(
+                System.getenv("DB_CONN"),
+                System.getenv("DB_USER"),
+                System.getenv("DB_PASS")
+        );
+
+        Statement statement = this.con.createStatement();
+
+        String query = "select * from TestTable";
+        if (this.filter != null){
+            query += " " + filter;
+        }
+
+        ResultSet resultSet = statement.executeQuery(query);
+
+        //loop thru the resultSet fetching each record
+        while (resultSet.next()) {
+            this.words.add(
+                new Word(
+                    resultSet.getString("id"),
+                    resultSet.getString("word")
+                )
+            );
+        }
+    }
+
+    public ArrayList<Word> getWords() {
+        return this.words;
+    }
+}
+
